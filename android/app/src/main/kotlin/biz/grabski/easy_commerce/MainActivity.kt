0aa81715aea14ede1cde564e2d2d47eaaafefa38
AGrabski <adam.gr@outlook.com>
2025-04-08 22:26:55 +0200

    feat: show feedback notification on app start
    - Add firebase appdistribution dependencies.
    - Show feedback notification on app start.

--- android/app/src/main/kotlin/biz/grabski/easy_commerce/MainActivity.kt
+++ android/app/src/main/kotlin/biz/grabski/easy_commerce/MainActivity.kt
@@ -1,5 +1,19 @@
 package biz.grabski.easy_commerce
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import com.google.firebase.ktx.*
+import com.google.firebase.appdistribution.*
+import com.google.firebase.appdistribution.ktx.*
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Firebase.appDistribution.showFeedbackNotification(
+            // Text providing notice to your testers about collection and
+            // processing of their feedback data
+            "Your feedback will be collected by firebase",
+            // The level of interruption for the notification
+            InterruptionLevel.HIGH)
+    }
+}
